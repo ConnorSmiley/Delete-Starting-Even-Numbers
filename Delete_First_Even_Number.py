b2c070f7ab06279c8a604e249cc75521df545093
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2021-01-25 13:22:12 +0900

    Add files via upload

--- Delete_First_Even_Number.py
+++ Delete_First_Even_Number.py
@@ -1,6 +1,13 @@
 delete_starting_evens = ([4, 8, 10, 11, 12, 15])
 lst = [4, 8, 10, 11, 12, 15]
 
+
+# def delete_starting_evens(lst):
+#   while (len(lst) > 0 and lst[0] % 2 == 0):
+#     lst = lst[1:]
+#   return lst
+
+
 # def delete_starting_evens(lst):
 #   for numbers in lst:
 #     if numbers % 2 == 0:
@@ -19,19 +26,19 @@ lst = [4, 8, 10, 11, 12, 15]
 #             break
 #     return new_lst
 
-def delete_starting_evens(lst):
-  for index in range(len(lst)):
-    while lst[index] % 2 == 0:
-      lst = lst[index+1:]
-      if lst[index] % 2 != 0:
-        break
-    return lst
-
 # def delete_starting_evens(lst):
-#   while lst[0] % 2 == 0 :
-#     del lst[0]  
-#     if lst == 0:
-#       break
-#   return lst
+#   for index in range(len(lst)):
+#     while lst[index] % 2 == 0:
+#       lst = lst[index+1:]
+#       if lst[index] % 2 != 0:
+#         break
+#     return lst
+
+def delete_starting_evens(lst):
+  while lst[0] % 2 == 0 :
+    del lst[0]  
+    if lst == 0:
+      break
+  return lst
 
 print(delete_starting_evens(lst))
